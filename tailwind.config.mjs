223ed5df3f144c8a2c82dfa8323a7f0a760029fa
asiasAtWork <asiasatwork@gmail.com>
2024-05-20 17:18:30 -0500

    Add icon packages, style nav and header components for desktop view

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+			  '.bg-overlay': {
+				'background': 'linear-gradient(var(--overlay-angle, 180deg), var(--overlay-colors)), var(--overlay-image)',
+				'background-position': 'center',
+				'background-size': 'cover',
+			  },
+			});
+		  }),
+	],
 }
