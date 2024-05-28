d3a0f5dc74205364da7b54158b28abc3d4d5bdb8
asiasAtWork <asiasatwork@gmail.com>
2024-05-28 07:43:00 -0500

    Attempt to fix MoreBlogPosts React component

--- src/components/MoreBlogPosts.jsx
+++ src/components/MoreBlogPosts.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState } from "react" 
+import BlogPost from "./BlogPost.astro"
 
 const moreBlogQuery = `
 query moreBlogQuery($cursor: String!, $size: Int!) {
@@ -37,27 +38,55 @@ query moreBlogQuery($cursor: String!, $size: Int!) {
     }
 }`
 
-function MoreBlogPosts({currentCursor, size=4, HYGRAPH_ENDPOINT}) {
+export default function MoreBlogPosts({currentCursor, size=4, HYGRAPH_ENDPOINT}) {
     const [posts, setPosts] = useState([])
     const [cursor, setCursor] = useState(currentCursor)
     const [hasNext, setHasNext] = useState(true)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
-    
-    //console.log('in the browser')
 
-    const getMorePosts = async () => {
+
+    const getMorePosts = () => {
+        setLoading(true)
+        console.log('in the browser')
+        /*
+        const response = await fetch(
+            HYGRAPH_ENDPOINT, 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    query: moreBlogQuery,
+                    variables: {
+                        size: size,
+                        cursor: cursor
+                    }
+                })
+            });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok...')
+        }
+
+        if (response) {
+            const result = await response.json();
+            console.log(result)
+        }
+        */
+    
 
     }
 
     return (
         <>
             <h1>More Blog Content in Progress...</h1>
+
             {loading && <div className="bg-emerald-950 p-4 text-slate-200 text-center">Loading...</div>}
-            {hasNext && <button className="bg-emerald-950 p-4 text-slate-200 text-center" onClick={getMorePosts()}>More Posts</button>}
+            {hasNext && <button className="bg-emerald-950 p-4 text-slate-200 text-center" onClick={getMorePosts}>More Posts</button>}
             {error && <div>Error: {error}</div>}
         </>
     )
 }
-
-export default MoreBlogPosts
